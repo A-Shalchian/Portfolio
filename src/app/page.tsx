cc204fb7ed6d3899cffb5c2931ca86f1e30ce661
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-10-14 00:57:51 -0400

    added the tape section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
-
+import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";
 export default function Home() {
   return (
     <div>
       <Header></Header>
       <HeroSection></HeroSection>
       <ProjectsSection></ProjectsSection>
+      <TapeSection></TapeSection>
+      <TestimonialsSection></TestimonialsSection>
     </div>
   );
 }
