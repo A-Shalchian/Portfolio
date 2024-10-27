6b4327efb2d37b775616b1af994076530b6ccaa6
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-10-27 17:46:55 -0400

    Added animations

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,6 @@
+"use client";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
-import StarIcon from "@/assets/icons/star.svg";
 import bookImage from "@/assets/images/book-cover.png";
 import Image from "next/image";
 import JavascriptIcon from "@/assets/icons/square-js.svg";
@@ -9,11 +9,12 @@ import GitHubIcon from "@/assets/icons/github.svg";
 import TailWindCSSIcon from "@/assets/icons/tailwind.svg";
 import MongoDBIcon from "@/assets/icons/mongodb-leaf.svg";
 import NodeJSIcon from "@/assets/icons/nodejsDark.svg";
-import { TechIcon } from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.png";
 import smileMemoji from "@/assets/images/memoji-smile.png";
 import { CardHeader } from "@/components/CardHeader";
 import { ToolboxItems } from "@/components/ToolboxItems";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const toolboxItems = [
   {
@@ -86,6 +87,7 @@ const hobbies = [
 ];
 
 export const AboutSection = () => {
+  const constraintRef = useRef(null);
   return (
     <div className="py-20 lg:py-28">
       <div className="container">
@@ -114,11 +116,15 @@ export const AboutSection = () => {
                 description="Explore the techs and tools I use to create digital experiences"
                 className=""
               />
-              <ToolboxItems items={toolboxItems} className="" />
+              <ToolboxItems
+                items={toolboxItems}
+                className=""
+                itemsWrapperClassName="animate-move-left [animation-duration:15s]"
+              />
               <ToolboxItems
                 items={toolboxItems}
                 className="mt-6"
-                itemsWrapperClassName="-translate-x-1/2"
+                itemsWrapperClassName="animate-move-right [animation-duration:15s]"
               />
             </Card>
           </div>
@@ -132,9 +138,9 @@ export const AboutSection = () => {
                 description="Explore my interests and hobbies beyond the code"
                 className="px-6 py-6"
               />
-              <div className="relative flex-1">
+              <div className="relative flex-1" ref={constraintRef}>
                 {hobbies.map((hobby) => (
-                  <div
+                  <motion.div
                     key={hobby.title}
                     className="inline-flex items-center gap-2 px-6 
                   bg-gradient-to-r from-emerald-300
@@ -143,12 +149,14 @@ export const AboutSection = () => {
                       left: hobby.left,
                       top: hobby.top,
                     }}
+                    drag
+                    dragConstraints={constraintRef}
                   >
                     <span className="font-semibold text-gray-950">
                       {hobby.title}
                     </span>
                     <span>{hobby.emoji}</span>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </Card>
@@ -162,11 +170,20 @@ export const AboutSection = () => {
               <div
                 className="absolute top-1/2 left-1/2 
             -translate-x-1/2 -translate-y-1/2 size-20
-            rounded-full bg-gradient-to-r from-emerald-300
-             to-sky-400 after:content-[''] after:absolute after:inset-0
+            rounded-full after:content-[''] after:absolute after:inset-0
              after:outline after:outline-2 after:-outline-offset-2
              after:rounded-full after:outline-gray-950/30"
               >
+                <div
+                  className="absolute inset-0 rounded-full
+                bg-gradient-to-r from-emerald-300
+             to-sky-400 -z-20 animate-ping [animation-duration:2s]"
+                ></div>
+                <div
+                  className="absolute inset-0 rounded-full
+                bg-gradient-to-r from-emerald-300
+             to-sky-400 -z-10"
+                ></div>
                 <Image
                   src={smileMemoji}
                   alt="smiling emoji"
