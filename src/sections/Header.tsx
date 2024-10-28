0917f063bb01db38e16e37fd077e4fb309add7c8
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-10-28 11:49:16 -0400

    fixed navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,18 +2,27 @@ export const Header = () => {
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item">
+        <a
+          href="#home"
+          className="nav-item hover:bg-white hover:text-gray-900 transition-colors duration-300 ease-in-out target:bg-white target:text-gray-900"
+        >
           Home
         </a>
-        <a href="#" className="nav-item">
+        <a
+          href="#projects"
+          className="nav-item hover:bg-white hover:text-gray-900 transition-colors duration-300 ease-in-out target:bg-white target:text-gray-900"
+        >
           Projects
         </a>
-        <a href="#" className="nav-item">
+        <a
+          href="#about"
+          className="nav-item hover:bg-white hover:text-gray-900 transition-colors duration-300 ease-in-out target:bg-white target:text-gray-900"
+        >
           About
         </a>
         <a
-          href="#"
-          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+          href="#contact"
+          className="nav-item hover:bg-white hover:text-gray-900 transition-colors duration-300 ease-in-out target:bg-white target:text-gray-900"
         >
           Contact
         </a>
