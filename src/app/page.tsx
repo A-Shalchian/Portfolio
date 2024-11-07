3e91d973996bcd60650271606c6aa23404c7c348
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-07 13:21:10 -0500

    removed the testimonials for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
-import { TestimonialsSection } from "@/sections/Testimonials";
+//import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
@@ -18,7 +18,7 @@ export default function Home() {
         <ProjectsSection></ProjectsSection>
       </section>
       <TapeSection></TapeSection>
-      <TestimonialsSection></TestimonialsSection>
+      {/* <TestimonialsSection></TestimonialsSection> */}
       <section id="about">
         <AboutSection></AboutSection>
       </section>
