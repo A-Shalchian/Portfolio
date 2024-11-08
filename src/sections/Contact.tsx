8a6d2a9ec5c55288691dbf871da183592a50ff54
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-08 00:52:52 -0500

    added linkedin to the contact me

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -27,13 +27,15 @@ export const ContactSection = () => {
               </p>
             </div>
             <div>
-              <button
-                className="text-white bg-gray-900 inline-flex
+              <a href="https://www.linkedin.com/in/arash-shalchian-230b06268/">
+                <button
+                  className="text-white bg-gray-900 inline-flex
           items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900"
-              >
-                <span className="font-semibold">Contact me</span>
-                <ArrowUpRightIcon className="size-4" />
-              </button>
+                >
+                  <span className="font-semibold">Contact me</span>
+                  <ArrowUpRightIcon className="size-4" />
+                </button>
+              </a>
             </div>
           </div>
         </div>
