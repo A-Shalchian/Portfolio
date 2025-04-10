fad7b971e204c36a025da6d2ec1f39ad0e79b3d8
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-04-09 21:14:12 -0400

    added resume

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,5 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-down.svg";
+
 export const Header = () => {
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
@@ -25,6 +27,14 @@ export const Header = () => {
           className="nav-item hover:bg-white hover:text-gray-900 transition-colors duration-300 ease-in-out target:bg-white target:text-gray-900"
         >
           Contact
+        </a>
+        <a
+          href="https://drive.google.com/file/d/12B62nEyRKasqH7VWBhoB76J6cg26Lu8b/view?usp=sharing"
+          className="nav-item hover:bg-white hover:text-gray-900 transition-colors duration-300 ease-in-out target:bg-white target:text-gray-900"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
         </a>
       </nav>
     </div>
