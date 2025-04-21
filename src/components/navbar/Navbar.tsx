6bceac9a62dd58a30d5710461a65586d48971eee
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-04-21 17:31:42 -0400

    refactor Navbar component for improved layout and styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,37 @@
+"use client";
+import { useState } from "react";
+import { Home, Code } from "lucide-react";
+import Link from "next/link";
+
 export const Navbar = () => {
+  const [activeItem, setActiveItem] = useState("/");
+
   return (
-    <nav className="fixed top-0 left-0 h-full w-32 bg-emerald-500">
-      <div className="flex flex-col items-center justify-evenly h-full">
-        <a href="" className="text-white text-lg font-bold">
-          Home
-        </a>
-        <a href="/projects" className="text-white text-lg font-bold">
-          Projects
-        </a>
-        <a href="/contact" className="text-white text-lg font-bold">
-          Contact
-        </a>
-        <a href="/experience" className="text-white text-lg font-bold">
-          Experience
-        </a>
-      </div>
-    </nav>
+    <div className="w-full flex justify-center items-center top-3 fixed z-10">
+      <nav className="flex p-0.5 border border-white/15 rounded-full bg-gray-400 backdrop-blur">
+        <Link
+          href="/"
+          className={`nav-item transition-colors duration-300 ease-in-out ${
+            activeItem === "/"
+              ? "bg-white text-gray-900"
+              : "hover:bg-gray-500 hover:text-gray-900"
+          }`}
+          onClick={() => setActiveItem("/")}
+        >
+          <Home />
+        </Link>
+        <Link
+          href="/projects"
+          className={`nav-item transition-colors duration-300 ease-in-out ${
+            activeItem === "projects"
+              ? "bg-white text-gray-900"
+              : "hover:bg-gray-500 hover:text-gray-900"
+          }`}
+          onClick={() => setActiveItem("projects")}
+        >
+          <Code />
+        </Link>
+      </nav>
+    </div>
   );
 };
