36e6a7682abf98a67c2c9b4e1c0666212646b89a
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-12-16 19:27:27 -0500

    removed the analytics

--- src/lib/kv.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@vercel/kv';
-
-export const kv = createClient({
-  url: process.env.analytics_KV_REST_API_URL!,
-  token: process.env.analytics_KV_REST_API_TOKEN!,
-});
